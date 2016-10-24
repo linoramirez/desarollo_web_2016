90a22b7083afde275db67c3348ff20a6f04b9d70
Linoek <lino.ramirez@hotmail.com>
2016-10-24 17:58:01 -0600

    V1.1 - Se agrego tabla numerica con javascript

--- ejercicio1/js/scripts.js
+++ ejercicio1/js/scripts.js
@@ -1,7 +1,6 @@
 // JavaScript Document
 function mostrarNumeros(){
-	var n = 0;
-	for (n; n + 1; n++){
-		document.getElementById("numeros")="<tr><td>"+n+"</td></tr>";
+	for (var n=0; n <= 10; n++){
+		document.getElementById("tablaNumeros").innerHTML  += "<tr><td>"+n+"</td></tr>";
 	}
 }
